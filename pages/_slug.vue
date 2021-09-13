22895a34a7a51936e1035f43b3364dfabf0234a8
Morten Sørensen <morten@moso.io>
2021-09-13 18:34:54 +0200

    Updated meta tags

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -18,8 +18,12 @@ export default {
         return {
             title: `${this.article.title} - moso.io`,
             meta: [
+                { hid: 'description', name: 'description', content: this.article.description },
+                { hid: 'apple-mobile-web-app-title', name: 'apple-mobile-web-app-title', content: this.article.title },
                 { hid: 'og:title', property: 'og:title', content: this.article.title },
-                { hid: 'twitter:title', property: 'twitter:title', content: this.article.title }
+                { hid: 'og:description', property: 'og:description', content: this.article.description },
+                { hid: 'twitter:title', name: 'twitter:title', content: this.article.title },
+                { hid: 'twitter:description', name: 'twitter:description', content: this.article.description }
             ]
         }
     }
