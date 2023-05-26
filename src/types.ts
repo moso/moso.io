c1f5ca0e8b6eee4aefbd931646ad4e2e7eba378f
moso <morten@moso.io>
2023-05-26 18:45:40 +0200

    Linted types

--- src/types.ts
+++ src/types.ts
@@ -3,21 +3,21 @@ import { ViteSSGContext } from 'vite-ssg';
 export type UserModule = (ctx: ViteSSGContext) => void;
 
 export type Education = {
-    title: string,
-    period: string,
+    title: string;
+    period: string;
 };
 
 export type Experience = {
-    title: string,
-    period: string,
-    location: string,
+    title: string;
+    period: string;
+    location: string;
 };
 
 export type Project = {
-    title: string,
-    githubLink?: string,
-    githubShort?: string,
-    url?: string,
-    description: string,
-    tags?: Array<string>,
+    title: string;
+    githubLink?: string;
+    githubShort?: string;
+    url?: string;
+    description: string;
+    tags?: Array<string>;
 };
