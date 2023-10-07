68ae6fd055646d8af400ba8d4c6c821378cf63cb
moso <morten@moso.io>
2023-10-07 10:08:44 +0200

    Clean-up

--- src/shims-svg.d.ts
+++
@@ -1,6 +0,0 @@
-// Import svg files as components
-declare module '*.svg' {
-    import Vue, { VueConstructor } from 'vue';
-    const content: VueConstructor<Vue>;
-    export default content;
-};
--- src/shims-vue.d.ts
+++
@@ -1,6 +0,0 @@
-// Import Vue components everywhere - in this instance in markdown
-declare module '*.vue' {
-    import { defineComponent } from 'vue';
-    const Component: ReturnType<typeof defineComponent>;
-    export default Component;
-};
