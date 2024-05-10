d86dd88e064310f125f224ccafd7975b0dfd5513
Morten Sørensen <morten@moso.io>
2024-05-10 13:58:49 +0200

    Test of headers log

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,27 @@
 <script setup lang="ts">
+import { onBeforeMount } from 'vue';
 import { useHead } from '@vueuse/head';
+
+// Test of getting the CSP headers
+const httpReq = new XMLHttpRequest();
+
+const parseHttpHeaders = (httpHeaders: any) => {
+    return httpHeaders.split('\n')
+        .map((x: any) => x.split(/: */,2))
+        .filter((x: any) => x[0])
+        .reduce((ac: any, x: any) => { ac[x[0]] = x[1]; return ac; }
+    );
+}
+
+onBeforeMount(() => {
+    httpReq.open('GET', document.location.toString(), true);
+    httpReq.send(null);
+    httpReq.onload = () => {
+        const headers = parseHttpHeaders(httpReq.getAllResponseHeaders());
+
+        console.log(headers['content-security-policy']);
+    };
+});
 
 useHead({
     meta: [
